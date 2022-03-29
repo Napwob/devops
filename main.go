506d3989e5c55a86fd908f46aa4b6a09ec9b835f
Napwob <47683903+Napwob@users.noreply.github.com>
2022-03-29 21:03:53 +0700

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
  
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!")
+		fmt.Fprintf(w, "Greetings, user!")
 	})
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, user!)")
